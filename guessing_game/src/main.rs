0d8ace2584a97cca5352e665290f67f3c1810dff
cg-cnu <sreenivas9alapati@gmail.com>
2017-10-21 23:48:29 +0530

    guessing game 1

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,31 @@
+// import io from the std (standard library)
+extern crate rand;
+use rand::Rng;
+use std::io;
+
+// function main with no input
+fn main() {
+    // print macros 
+    println!("Guess the number!");
+    println!("Please input your guess!");
+    // let creates a variable x
+    // mut makes it mutable, by default everything is immutable 
+    // String::new() returns a new instance of string
+    // `Sting` is string type provided by std
+    // `::` accesses the static methods on String
+    // `new` creates a new instance of String.  
+    let mut guess = String::new();
+    // io::stdin accesses the stdin function on the io
+    // io::stdin() creates a new instance of the stdin
+    // is a type that represents the standard input of your terminal
+    // read_line is the method wich takes the input and provide to the argument
+    // & will indicate that its a reference
+    // multiple parts of code can access the same variable 
+    // like variables reference  
+    io::stdin().read_line(&mut guess)
+        .expect("failed to read line!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
+    println!("You guessed: {}", guess);
+}
+
